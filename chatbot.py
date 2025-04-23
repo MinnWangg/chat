d2ef44f0347207165117d41ed9b1161844c54062
MinnWangg <3001nguyenquang@gmail.com>
2025-04-23 13:16:59 +0700

    lai 7

--- chatbot.py
+++ chatbot.py
@@ -155,4 +155,5 @@ def ask():
     return jsonify({"response": "Yêu cầu POST với câu hỏi."})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
